80f195939519a3f9cf4127666178bb559655f8e1
Christian Pedersen <Chrelle@tilux.dk>
2016-05-18 13:30:05 +0200

    autofix

--- code/src/master.cpp
+++ code/src/master.cpp
@@ -54,7 +54,7 @@ void *CV_avoid(void *arg)
    //------------- Create objects and variables -------------
    feature_detection FT;   // Feature Detection object - used for CV methods
    VideoCapture cap(0);       // Video Capture object - used to get frames from video
-   cap.set(CV_CAP_PROP_FPS , 60);
+   printf( cap.get(CV_CAP_PROP_FPS) );
 
    //------------- Variables --------------------------------
    int local_reaction = REACT_NOTHING;
